fde23e52cd880ccf6ef49d870c2e5c8a3d03b121
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-08-19 07:08:34 +0100

    Add project files.

---
+++ Generics_Assignment/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics_Assignment
+{
+    class Person
+    {
+        public int Id { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+    }
+}
---
+++ Generics_Assignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Generics_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee = new Employee<string>();
+            employee.Things = new System.Collections.Generic.List<string>();
+
+            employee.Things.Add("thing1");
+            employee.Things.Add("thing2");
+            employee.Things.Add("thing3");
+
+            Employee<int> employee1 = new Employee<int>();
+            employee1.Things = new System.Collections.Generic.List<int>();
+
+            employee1.Things.Add(123);
+            employee1.Things.Add(543);
+            employee1.Things.Add(767473);
+
+            foreach (string thing in employee.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            Console.WriteLine("\n");
+
+            foreach (int thing in employee1.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            Console.ReadLine();
+
+        }
+
+    }
+}
